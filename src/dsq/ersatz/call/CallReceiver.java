76da01e0783a785114ea4b3a34f5f6d48a5b7120
dsq <dsq@server>
2012-12-20 10:36:20 +1000

    call receiving

---
+++ src/dsq/ersatz/call/CallReceiver.java
@@ -0,0 +1,13 @@
+package dsq.ersatz.call;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CallReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+    }
+}
