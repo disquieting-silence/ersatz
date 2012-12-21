a706e0063188e709a822d2f3bd32ee74967f6f7f
dsq <dsq@server>
2012-12-21 16:15:46 +1000

    commandbar

---
+++ src/dsq/ersatz/ui/commandbar/ButtonIcon.java
@@ -0,0 +1,9 @@
+package dsq.ersatz.ui.commandbar;
+
+import android.app.Activity;
+import dsq.ersatz.action.SimpleAction;
+
+public interface ButtonIcon {
+    /* Dupe from listless. */
+    void setAction(SimpleAction action);
+}
---
+++ src/dsq/ersatz/ui/commandbar/DefaultButtonIcon.java
@@ -0,0 +1,42 @@
+package dsq.ersatz.ui.commandbar;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import dsq.ersatz.R;
+import dsq.ersatz.action.SimpleAction;
+
+public class DefaultButtonIcon extends LinearLayout implements ButtonIcon {
+
+    private final ImageButton button;
+
+    public DefaultButtonIcon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        final TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.CommandBarIconLayout);
+        final String value = attributes.getString(R.styleable.CommandBarIconLayout_icon);
+
+        button = new ImageButton(context);
+
+        final int drawable = context.getResources().getIdentifier(value, "drawable", context.getPackageName());
+        button.setBackgroundResource(drawable);
+
+        final LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        button.setLayoutParams(params);
+
+        addView(button);
+    }
+
+    /* FIX: Really wish this was an interface .... Dupe from listless. */
+    public void setAction(final SimpleAction action) {
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View view) {
+                action.run();
+            }
+        });
+    }
+}
